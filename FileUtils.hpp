dcdd1b200edbc9df2b279e8e3268b6f5a8207f4f
Sergey Rodin <rodin.s@rambler.ru>
2017-12-23 20:37:21 +0200

    Change saving directory to .gtk_online_life/saves

--- FileUtils.hpp
+++ FileUtils.hpp
@@ -3,13 +3,13 @@
 #include <glib/gstdio.h>
 #include <fstream>
 
-string homeAppDir = string(g_get_home_dir()) + "/.gtk_online_life";
+string homeAppSavesDir = string(g_get_home_dir()) + "/.gtk_online_life/saves";
 
 class FileUtils {
 	public:
 	
     static string readFromFile(string filename) { 
-		filename = homeAppDir + "/" + filename;
+		filename = homeAppSavesDir + "/" + filename;
 		ifstream in(filename.c_str());
 		if(in) {
 			return string((istreambuf_iterator<char>(in)), istreambuf_iterator<char>());
@@ -18,8 +18,8 @@ class FileUtils {
 	}
 	
 	static void writeToFile(string filename, string input) {
-		g_mkdir_with_parents(homeAppDir.c_str(), S_IRWXU); 
-		filename = homeAppDir + "/" + filename;
+		g_mkdir_with_parents(homeAppSavesDir.c_str(), S_IRWXU); 
+		filename = homeAppSavesDir + "/" + filename;
 		ofstream out(filename.c_str());
 		if(out) {
 			out << input;
