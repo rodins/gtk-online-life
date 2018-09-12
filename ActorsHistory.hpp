24a0b9b10f7306853cab7b366dded042bb7576b0
Sergey Rodin <rodin.s@rambler.ru>
2018-09-12 07:18:02 +0300

    Do not use ProcessPlayItemDialog class. Move functionality into ActorsHistory. Implement Actors button on play item dialog.

--- ActorsHistory.hpp
+++ ActorsHistory.hpp
@@ -2,7 +2,6 @@
 #include "Actors.hpp"
 #include "FileUtils.hpp"
 #include "ResultsHistory.hpp"
-#include "ProcessPlayItemDialog.hpp"
 
 class ActorsHistory {
     Actors actors, prevActors;
@@ -355,6 +354,117 @@ class ActorsHistory {
 		gtk_spinner_stop(GTK_SPINNER(spLinks));
 	}
 	
+	void playLink(string player, string link) {
+		string command = player + link + " &";
+		system(command.c_str());
+	}
+	
+	void createPlayItemDialog(PlayItem playItem, 
+	                          string sizeFile, string sizeDownload) {
+		const string FLV = "FLV", MP4 = "MP4", PLAY = "Play";
+		GtkWidget *label;
+		
+		// Create dialog, add buttons to it
+		GtkWidget *dialog,  
+		          *content_area;
+		
+		dialog = gtk_dialog_new_with_buttons ("Links dialog",
+                                  GTK_WINDOW(window),
+                                  (GtkDialogFlags)(GTK_DIALOG_MODAL|
+                                                   GTK_DIALOG_DESTROY_WITH_PARENT),
+                                  NULL);
+        string msg;
+		if(!playItem.player.empty()) {
+			msg = "Play in " + playItem.player + ". Copy to clipboard";
+		}else {
+			msg = "Mplayer or mpv not detected. Copy to clipboard";
+		}
+		
+		// Add actors button to dialog if actors is not displayed
+		if(!gtk_toggle_tool_button_get_active(
+		                GTK_TOGGLE_TOOL_BUTTON(btnActors))) {
+			gtk_dialog_add_button(GTK_DIALOG(dialog),
+		                          "Actors",
+                                  LINK_RESPONSE_INFO);
+		}                                      
+                           
+        if(!sizeFile.empty()) {
+			string sizeTitle = FLV + " (" + sizeFile + ")";
+			GtkWidget *btnFlv = gtk_dialog_add_button(GTK_DIALOG(dialog),
+		                               sizeTitle.c_str(),
+                                       LINK_RESPONSE_PLAY);
+            gtk_widget_set_tooltip_text(btnFlv, msg.c_str());
+		}
+        
+        if(!sizeDownload.empty()) {
+			// If btnFlv is not available, change button title to "Play"
+			string title;
+			if(sizeFile.empty()) {
+			    title = PLAY;	
+			}else {
+				title = MP4;
+			}
+		    string sizeTitle = title + " (" + sizeDownload + ")";
+			GtkWidget *btnMp4 = gtk_dialog_add_button(GTK_DIALOG(dialog),
+		                      sizeTitle.c_str(),
+                              LINK_RESPONSE_DOWNLOAD);
+            gtk_widget_set_tooltip_text(btnMp4, msg.c_str());
+		}
+        
+        gtk_dialog_add_button(GTK_DIALOG(dialog),
+		                      "Cancel",
+                              LINK_RESPONSE_CANCEL);
+                                              
+        content_area = gtk_dialog_get_content_area (GTK_DIALOG (dialog));
+        
+	    label = gtk_label_new (playItem.comment.c_str());
+	    //gtk_widget_set_size_request(label, 390, -1);
+        gtk_label_set_line_wrap(GTK_LABEL(label), TRUE);
+        //gtk_label_set_justify(GTK_LABEL(label), GTK_JUSTIFY_CENTER);
+	    
+	    /* Add the label, and show everything we've added to the dialog. */
+	    gtk_box_pack_start(GTK_BOX(content_area), label, TRUE, FALSE, 5);
+	    gtk_widget_show_all(content_area);
+		                   
+		int response_id = gtk_dialog_run(GTK_DIALOG(dialog));
+		
+		// For pasting with "paste" or ctrl-v
+        GtkClipboard* clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
+        // For pasting with middle mouse button (in urxvt)
+        GtkClipboard* clipboardX = gtk_clipboard_get(GDK_SELECTION_PRIMARY);
+        switch(response_id) {
+			case LINK_RESPONSE_PLAY:
+			    gtk_clipboard_set_text(clipboard,
+			                           playItem.file.c_str(),
+			                           playItem.file.size());
+			    gtk_clipboard_set_text(clipboardX,
+			                           playItem.file.c_str(),
+			                           playItem.file.size());
+			                           
+			    playLink(playItem.player, playItem.file);
+			break;
+			case LINK_RESPONSE_DOWNLOAD:
+			    gtk_clipboard_set_text(clipboard,
+			                           playItem.download.c_str(),
+			                           playItem.download.size());
+			    gtk_clipboard_set_text(clipboardX,
+			                           playItem.download.c_str(),
+			                           playItem.download.size());
+			    playLink(playItem.player, playItem.download);
+			break;
+			case LINK_RESPONSE_INFO:
+			    if(actors.isNetworkOk()) {
+					gtk_widget_show(vbRight);
+				}else if(!actors.getUrl().empty()) {
+					newActorsThread();
+				}
+			case LINK_RESPONSE_CANCEL:
+			    // Do nothing.
+			break;
+		}
+		gtk_widget_destroy(dialog);
+	}
+	
 	static void linksSizeTask(gpointer arg1, gpointer arg2) {
 		ActorsHistory *actorsHistory = (ActorsHistory *)arg2;
 		PlayItem *playItem = (PlayItem*)arg1;
@@ -376,10 +486,9 @@ class ActorsHistory {
 		if(sizeFile.empty() && sizeDownload.empty()) {
 			actorsHistory->runLinksErrorDialog();
 		}else {
-		    ProcessPlayItemDialog ppid(actorsHistory->window, 
-		                               *playItem, 
-		                               sizeFile,
-		                               sizeDownload);
+			actorsHistory->createPlayItemDialog(*playItem,
+			                                     sizeFile,
+			                                     sizeDownload);
 		}
 		gdk_threads_leave();
 	}
@@ -406,7 +515,8 @@ class ActorsHistory {
 				ListEpisodesArgs listEpisodesArgs;
 				listEpisodesArgs.title = actorsHistory->actors.getTitle();
 				listEpisodesArgs.playlist_link = playlist_link;
-				actorsHistory->resultsHistory->setListEpisodesArgs(listEpisodesArgs);
+				actorsHistory->resultsHistory->setListEpisodesArgs(
+				     listEpisodesArgs);
 				actorsHistory->resultsHistory->btnListEpisodesClicked();
             }
 		}else if(js.length() > 500) { // Movie
@@ -576,6 +686,9 @@ class ActorsHistory {
 				actors.setLinksMode(LINKS_MODE_HIDE);
 			}
 			updateActors();
+			// Need to do this if called from dialog
+			gtk_toggle_tool_button_set_active(
+				               GTK_TOGGLE_TOOL_BUTTON(btnActors), TRUE);
 			showActors();
 		}else {
 		    showActorsError();
