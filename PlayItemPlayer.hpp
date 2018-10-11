016db94637ed56a3c42c022a6d2396752da1e738
Sergey Rodin <rodin.s@rambler.ru>
2018-10-11 20:57:23 +0300

    Check for omxplayer first in case on Raspberry Pi would be installed something else'

--- PlayItemPlayer.hpp
+++ PlayItemPlayer.hpp
@@ -5,15 +5,15 @@ class PlayItemPlayer {
     string command;
     public:
     PlayItemPlayer() {
-		if(system("which mpv") == 0) {
+		if(system("which omxplayer") == 0) { // Raspberry Pi option
+			player = "omxplayer";
+			command = "lxterminal -e " + player + " -b ";
+		}else if(system("which mpv") == 0) {
 			player = "mpv";
 			command = player + " --cache=2048 ";
 		}else if(system("which mplayer") == 0) {
 			player = "mplayer";
 			command = player + " -cache 2048 ";
-		}else if(system("which omxplayer") == 0) { // Raspberry Pi option
-			player = "omxplayer";
-			command = "lxterminal -e " + player + " -b ";
 		}
 	}
 	
