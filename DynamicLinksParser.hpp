02d8ac3e42d703c3df84665fa10006d9c30eaf75
Sergey Rodin <rodin.s@rambler.ru>
2018-10-29 05:09:59 +0200

    DynamicParser fix for trailers https links

--- DynamicLinksParser.hpp
+++ DynamicLinksParser.hpp
@@ -3,11 +3,11 @@
 class DynamicLinksParser {
 	public:
     static string parsePlayerForUrl(string &player) {
-		size_t begin = player.find("http%3A");
-		size_t end = player.find("\"", begin);
+		size_t begin = player.find("ref_url:");
+		size_t end = player.find("\"", begin+15);
 		if(begin != string::npos && end != string::npos) {
-			size_t length = end - begin;
-		    return player.substr(begin, length);
+			size_t length = end - begin - 10;
+		    return player.substr(begin+10, length);
 		}
 		return "";
 	}
